ba1a29c3901d4412ced5a89788e16988bc20856f
Thaife <tranvanthai17092001@gmail.com>
2021-12-22 12:03:07 +0700

    push day1-time1

---
+++ MISA.Fresher.Web11/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Fresher.Web11.Model;
+
+namespace MISA.Fresher.Web11.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet("{Number}/{Name}")]
+        public int? GetNumber(int? Number, string Name)
+        {
+            return Number;
+        }
+
+        [HttpGet("search")]
+        public string GetName(string name)
+        {
+            return name;
+        }
+
+        [HttpPost]
+        public object Post(Customer customer, string? name)
+        {
+            return new
+            {
+                Name = name,
+                Data = customer
+            };
+        }
+
+        [HttpPut]
+        public Customer Put(Customer customer)
+        {
+            return customer;
+        }
+    }
+}
---
+++ MISA.Fresher.Web11/Model/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MISA.Fresher.Web11.Model
+{
+    public class Customer
+    {
+        public string CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
